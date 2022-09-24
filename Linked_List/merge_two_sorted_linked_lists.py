a0745f14e45ee777f019d25ec0664dc1fddfed8b
Rohan Sharma <68840167+Sustainability4@users.noreply.github.com>
2022-09-24 13:32:33 +0530

    Create merge_two_sorted_linked_lists.py
    Merge two sorted linked lists and return it as a new list. 
    
    The new list should be made by splicing together the nodes of the first two lists, and should also be sorted.
    
    For example, given following linked lists :
    
      5 -> 8 -> 20 
      4 -> 11 -> 15
    The merged list should be :
    
    4 -> 5 -> 8 -> 11 -> 15 -> 20

---
+++ Linked_List/merge_two_sorted_linked_lists.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#	def __init__(self, x):
+#		self.val = x
+#		self.next = None
+
+class Solution:
+	# @param A : head node of linked list
+	# @param B : head node of linked list
+	# @return the head node in the linked list
+    
+    # We kept storing values in a list depending upon the sorted order for both the combined linked lists 
+    # Time Complexity was O(n+m) where n is length of linked list A and m is length of linked list B 
+	def mergeTwoLists(self, A, B):
+        list_number = []
+        
+        while A and B:
+            if A.val == B.val:
+                list_number.append(A.val)
+                list_number.append(B.val)
+                A = A.next 
+                B = B.next 
+            
+            elif A.val > B.val:
+                list_number.append(B.val)
+                B = B.next 
+            
+            elif B.val> A.val:
+                list_number.append(A.val)
+                A = A.next
+                
+            
+        if A == None and B != None:
+            while B :
+                list_number.append(B.val)
+                B = B.next
+        
+        if A!= None and B == None:
+            while A:
+                list_number.append(A.val)
+                A = A.next 
+        
+        root = ListNode(list_number[0])
+        
+        temp = root 
+        
+        for i in range(1,len(list_number)):
+            temp.next = ListNode(list_number[i])
+            temp = temp.next 
+        
+        return root 
+        
