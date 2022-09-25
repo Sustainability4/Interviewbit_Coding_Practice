4c30d71497be72c312b6db817fb5d9ee95e9de29
Rohan Sharma <68840167+Sustainability4@users.noreply.github.com>
2022-09-25 11:14:32 +0530

    Create crossing_bridge.py

---
+++ Puzzles/crossing_bridge.py
@@ -0,0 +1,26 @@
+'''
+Problem is that it’s dark and so you can’t cross the bridge without a flashlight and they only have one flashlight. 
+
+Plus the bridge is only big enough for two people to cross at once. 
+
+The four people walk at different speeds:
+
+one fella is so fast it only takes him 1 minute to cross the bridge,
+another 2 minutes,
+a third 5 minutes,
+the last it takes 10 minutes to cross the bridge.
+When two people cross the bridge together (sharing the flashlight), they both walk at the slower person’s pace. What is the minimum time required for all 4 to cross the bridge.
+
+Respond with a number which represents the number of minutes.
+
+Answer is a integer.  Just put the number without any decimal places if its an integer. If the answer is Infinity, output Infinity.
+
+Feel free to get in touch with us if you have any questions
+'''
+'''
+Answer = 17
+on first , 1 and 2 will pass the bridge and then 1 will come back = 2+1 =3 mins,
+on second ,5 and 10 will pass the bridge and 2 will come back = 10+2=12mins
+on third 1 and 2 will pass the bridge = 2mins 
+total time = 3+12+2=17 mins
+'''
